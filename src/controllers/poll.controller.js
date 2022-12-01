314e9ab94e0c5e690e1b88d2573ff591195a2a66
gustavo de assis <gustavo.siqueira.421@gmail.com>
2022-12-01 15:06:25 -0300

    feat: rota get/poll

--- src/controllers/poll.controller.js
+++ src/controllers/poll.controller.js
@@ -19,3 +19,13 @@ export async function postPoll(req, res){
         res.status(500).send("Couldn't create poll!");
     }
 }
+
+export async function getPoll(req, res){
+    try{
+        const polls = await pollCollection.find().toArray();
+        res.send(polls).status(200);
+        
+    } catch (err){
+        res.sendStatus(500);
+    }
+}
--- src/routes/poll.route.js
+++ src/routes/poll.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { postPoll } from "../controllers/poll.controller.js";
+import { getPoll, postPoll } from "../controllers/poll.controller.js";
 import { pollValidation } from "../middlewares/poll.middleware.js";
 
 const router = Router();
 
 router.post("/poll", pollValidation, postPoll);
+router.get("/poll", getPoll);
 
 
 export default router;
