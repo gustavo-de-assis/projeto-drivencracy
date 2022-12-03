c9e399bfc481889453b6883e353049ca9ed3c273
gustavo de assis <gustavo.siqueira.421@gmail.com>
2022-12-03 17:59:18 -0300

    feat: validacao post/choice. existe poll?

--- src/controllers/choice.controller.js
+++ src/controllers/choice.controller.js
@@ -1,13 +1,20 @@
-import { choiceCollection } from "../database/db.js";
+import { ObjectId } from "mongodb";
+import { choiceCollection, pollCollection } from "../database/db.js";
 
 export async function postChoice(req, res){
     const newChoice = req.body;
 
     try{
         const choice = await choiceCollection.findOne({ title: newChoice.title });
-
+        
         if (choice) {
             return res.status(409).send("Choice already exists!");
+        }
+        
+        const poll = await pollCollection.findOne({_id: ObjectId(newChoice.pollId)});
+        
+        if(!poll){
+            return res.status(404).send("Poll not found!");
         }
 
         await choiceCollection.insertOne({...newChoice});
