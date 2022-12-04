44893aaea5bbf203f36a9b62ee6cb3fd9fa8db44
gustavo de assis <gustavo.siqueira.421@gmail.com>
2022-12-04 17:06:41 -0300

    fix: votes com formato correto do obj

--- src/controllers/choice.controller.js
+++ src/controllers/choice.controller.js
@@ -2,55 +2,60 @@ import dayjs from "dayjs";
 import { ObjectId } from "mongodb";
 import { choiceCollection, pollCollection, voteCollection } from "../database/db.js";
 
-export async function postChoice(req, res){
+export async function postChoice(req, res) {
     const newChoice = req.body;
 
-    try{
+    try {
         const choice = await choiceCollection.findOne({ title: newChoice.title });
 
         if (choice) {
             return res.status(409).send("Choice already exists!");
         }
-        
-        const poll = await pollCollection.findOne({_id: ObjectId(newChoice.pollId)});
-        
-        if(!poll){
+
+        const poll = await pollCollection.findOne({ _id: ObjectId(newChoice.pollId) });
+
+        if (!poll) {
             return res.status(404).send("Poll not found!");
         }
-        
-        if(dayjs().isAfter(poll.expireAt)){
+
+        if (dayjs().isAfter(poll.expireAt)) {
             return res.status(403).send("Poll already expired!");
         }
 
-        await choiceCollection.insertOne({...newChoice});
-        await voteCollection.insertOne({...newChoice, votes: 0})
+        await choiceCollection.insertOne({ ...newChoice });
+        /*await voteCollection.insertOne({...newChoice, votes: 0})*/
 
         res.sendStatus(201);
 
-    }catch (err){
+    } catch (err) {
         console.log(err);
         res.status(500).send("Couldn't create choice!");
     }
 }
 
-export async function voteChoice(req, res){
+export async function voteChoice(req, res) {
     const id = req.params.id;
-    const choice = await voteCollection.findOne({_id: new ObjectId(id)})
+    const choice = await choiceCollection.findOne({ _id: new ObjectId(id) })
 
-    try{
+    try {
         //Existe a opcao?
-        if(!choice){
+        if (!choice) {
             return res.status(404).send("Not Found!");
         }
 
-        const poll = await pollCollection.findOne({_id: new ObjectId(choice.pollId)});
+        const poll = await pollCollection.findOne({ _id: new ObjectId(choice.pollId) });
 
         //Enquete expirada?
-        if(dayjs().isAfter(poll.expireAt)){
+        if (dayjs().isAfter(poll.expireAt)) {
             return res.status(403).send("Poll already expired!");
         }
 
-        let updateVote = Number(choice.votes);
+        await voteCollection.insertOne({
+            createdAt: dayjs().format('YYYY/MM/DD HH:mm:ss'),
+            choiceId: new ObjectId(id)
+        })
+
+        /*let updateVote = Number(choice.votes);
         isNaN(updateVote)? updateVote = 1 : updateVote++;
 
         voteCollection.updateOne({_id: new ObjectId(id)}, 
@@ -58,10 +63,10 @@ export async function voteChoice(req, res){
 
         voteCollection.updateOne({_id: new ObjectId(id)}, {$push:{date: dayjs().format('YYYY/MM/DD HH:mm:ss')}})
 
-        console.log(choice);
+        console.log(choice);*/
 
         return res.sendStatus(200);
-    } catch (err){
+    } catch (err) {
         res.sendStatus(500);
     }
 
